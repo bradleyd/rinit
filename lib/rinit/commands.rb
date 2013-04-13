a38fff5944b05a7117d0d6a14742f02e0437eb1b
Bradley Smith <bradleydsmith@gmail.com>
2013-04-12 20:09:10 -0400

    add mocha and sys proctable

--- lib/rinit/commands.rb
+++ lib/rinit/commands.rb
@@ -1,5 +1,8 @@
+require 'sys/proctable'
+
 module Rinit
   class << self
+    include Sys
     def get_pid_from_file filename
       begin
         pid = IO.readlines(filename).first
@@ -17,8 +20,14 @@ module Rinit
 
     end
 
-    def status
-
+    def status ppid
+      status = "stopped"
+      ProcTable.ps{ |p|
+        if p.pid == ppid
+          status = "running"
+        end
+      }
+      status
     end
 
     def restart
--- test/rinit_commands_test.rb
+++ test/rinit_commands_test.rb
@@ -1,9 +1,16 @@
 require_relative "test_helper"
+require 'tempfile'
+require 'sys/proctable'
 
 class RinitCommandsTest < MiniTest::Unit::TestCase
+  extend Rinit
+  include Sys
 
   def setup
     @rinit = Rinit
+    @pidfile = Tempfile.new('tempfile')
+    @pidfile.write(1)
+    @pidfile.close
   end
 
   def test_that_respond_to_start
@@ -18,8 +25,19 @@ class RinitCommandsTest < MiniTest::Unit::TestCase
     assert_respond_to @rinit, :status
   end
 
+  def test_status_should_be_stopped
+    ProcTable.expects(:ps).returns("stopped")   
+    assert_equal @rinit.status(1), "stopped"  
+  end
+
   def test_that_respond_to_restart
     assert_respond_to @rinit, :restart
   end
+
+  def test_get_pid_from_file
+    pid = Rinit.get_pid_from_file @pidfile
+    assert_equal pid, "1"
+  end
+
 
 end
