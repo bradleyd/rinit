e8ade1aaf2056fdda9ae6796411d343bdf87f783
Bradley Smith <bradleydsmith@gmail.com>
2013-04-26 23:42:09 -0400

    update test for template

--- test/template_builder_test.rb
+++ test/template_builder_test.rb
@@ -1,19 +1,29 @@
 require_relative "test_helper"
-require 'tempfile'
-require 'sys/proctable'
-require "ostruct"
+require_relative './test_helper'
 
 class RinitTemplateBuilderTest < MiniTest::Unit::TestCase
  
   def setup
-    @generator = Rinit::TemplateBuilder.new(template: 'init', name: 'foobar')
+    @name = 'foobar'
+    @template = 'init'
+    @example_file = File.join(File.expand_path(File.dirname(__FILE__)), "../", 
+                              "#{@name}.#{@template}")   
+    @generator = Rinit::TemplateBuilder.new(template: @template, name: @name)
   end
 
   def test_should_respond_to_build
     assert_respond_to(@generator, :build)
   end
 
-  def test_should_print_out_template
-    assert_match(@generator.build, /APP_NAME foobar/)
+  def test_should_create_template
+    @generator.build
+    assert(File.exist?(@example_file), "cant find file")
+  end
+
+  def teardown
+    begin
+      FileUtils.rm(@example_file)
+    rescue
+    end
   end
 end
